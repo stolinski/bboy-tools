dfe5f660ec94b21ccabdec3cb516dbd29a24adcd
stolinski <scotttolinski@gmail.com>
2016-12-30 16:48:11 -0700

    tweaks js file

--- client/routes.jsx
+++ client/routes.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {mount} from 'react-mounter';
+import { mount } from 'react-mounter';
 
-import {HomeLayout} from './layout/HomeLayout.jsx';
-import {MainLayout} from './layout/MainLayout.jsx';
+import { HomeLayout } from './layout/HomeLayout.jsx';
+import { MainLayout } from './layout/MainLayout';
 import Home from './layout/Home';
 import App from './moves/App.jsx';
 import PracticeTools from './tools/PracticeTools.jsx';
@@ -10,69 +10,69 @@ import Thirties from './tools/Thirties.jsx';
 import BattleMode from './tools/BattleMode.jsx';
 
 FlowRouter.route('/', {
-    name: 'home',
-    action() {
-        if (Meteor.userId()) {FlowRouter.go('moves');}
-        mount(HomeLayout, {
-            content: () => (<Home />)
-        });
-    }
+  name: 'home',
+  action() {
+    if (Meteor.userId()) { FlowRouter.go('moves'); }
+    mount(HomeLayout, {
+      content: () => (<Home />),
+    });
+  },
 });
 
 FlowRouter.route('/moves', {
-    name: 'moves',
-    action() {
-        if (!Meteor.userId()) {FlowRouter.go('home');}
-        mount(MainLayout, {
-            content: () => (<App />)
-        });
-    }
+  name: 'moves',
+  action() {
+    if (!Meteor.userId()) { FlowRouter.go('home'); }
+    mount(MainLayout, {
+      content: () => (<App />),
+    });
+  },
 });
 
 const practiceTools = FlowRouter.group({
-    prefix: '/practice-tools'
+  prefix: '/practice-tools',
 });
 
 practiceTools.route('/', {
-    action() {
-        mount(MainLayout, {
-            content: () => (<PracticeTools />)
-        });
-    }
+  action() {
+    mount(MainLayout, {
+      content: () => (<PracticeTools />),
+    });
+  },
 });
 
 practiceTools.route('/thirties', {
-    action() {
-        mount(MainLayout, {
-            content: () => (<Thirties />)
-        });
-    }
+  action() {
+    mount(MainLayout, {
+      content: () => (<Thirties />),
+    });
+  },
 });
 
 practiceTools.route('/one-in-the-chamber', {
-    action() {
-        mount(MainLayout, {
-            content: () => (<Thirties />)
-        });
-    }
+  action() {
+    mount(MainLayout, {
+      content: () => (<Thirties />),
+    });
+  },
 });
 
 
 const battleTools = FlowRouter.group({
-    prefix: '/battle-tools'
+  prefix: '/battle-tools',
 });
 
 battleTools.route('/', {
-    action() {
-        mount(MainLayout, {
-            content: () => (<BattleTools />)
-        });
-    }
+  action() {
+    mount(MainLayout, {
+      content: () => (<BattleTools />),
+    });
+  },
 });
 battleTools.route('/battle-mode', {
-    action() {
-        mount(MainLayout, {
-            content: () => (<BattleMode />)
-        });
-    }
+  action() {
+    mount(MainLayout, {
+      content: () => (<BattleMode />),
+    });
+  },
 });

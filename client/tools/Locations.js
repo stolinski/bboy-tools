af7c1213ba7d4926f5235c4c4c437494cfbdc521
Oliver Xu <oxu_e@hotmail.com>
2017-06-05 00:49:40 -0400

    locations route

---
+++ client/tools/Locations.js
@@ -0,0 +1,20 @@
+import React, { PureComponent } from 'react';
+import { autobind } from 'core-decorators';
+
+@autobind
+export default class Locations extends PureComponent {
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="comboizer practice-tools types">
+        <h1>Location</h1>
+        <div>
+          
+        </div>
+      </div>
+    );
+  }
+}

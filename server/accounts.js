fe60751fd544ac8ae6c020b1e2b29ee8d925a50f
Oliver Xu <oxu_e@hotmail.com>
2017-06-14 02:09:34 -0400

    add default features to new accounts

--- server/accounts.js
+++ server/accounts.js
@@ -1,3 +1,18 @@
+const DEFAULT_FEATURES = ['moves', 'practice-tools', 'battle-tools'];
+
+activateDefaultFeatures = function(user){
+  user.features = [];
+  DEFAULT_FEATURES.forEach(function(feature){
+    user.features.push(feature);
+  })
+}
+
+Accounts.onCreateUser(function(options, user){
+  console.log("ASD");
+  activateDefaultFeatures(user);
+  return user;
+})
+
 activateFeature = function(user, feature){
   console.log("Adding " + feature + " to user with id " + user._id);
   Meteor.users.update(
